8946531ef1a665a7e099a519b956d4ac9f90a59b
Mark Kozlov <37026894+Ubermensch124@users.noreply.github.com>
2021-12-24 20:18:33 +0300

    Update __init__.py

--- optimizationalgo/optimizationalgo/__init__.py
+++ optimizationalgo/optimizationalgo/__init__.py
@@ -1,2 +1,15 @@
+"""
+``optimizationalgo``
+=====================
+
+Библиотека, реализующая оптимизационные алгоритмы для задач разного рода.
+В данный момент поддерживаются алгоритмы для задачи коммовояжёра.
+Для подробной информации пройдите в optimizationalgo.voyage
+
+"""
 from .voyage import simulated_annealing
 from .voyage import ants_colony
+
+__author__ = 'Mark Kozlov'
+
+__version__ = "0.0.7"
