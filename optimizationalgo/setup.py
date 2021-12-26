556619b629d27e941c8498ffbab1d3004b11061e
Mark Kozlov <37026894+Ubermensch124@users.noreply.github.com>
2021-12-26 16:58:24 +0300

    0.0.10

--- optimizationalgo/setup.py
+++ optimizationalgo/setup.py
@@ -6,13 +6,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="optimizationalgo",
     packages=['optimizationalgo'],
-    version="0.0.4",
+    version="0.0.10",
     description='Optimization algorithms for the traveling salesman problem',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Mark Kozlov",
     author_email="mark.k.2012@yandex.ru",
     url="https://github.com/SMALA-comand/Optimization_algo",
+    install_requires=['networkx', 'matplotlib'],
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
