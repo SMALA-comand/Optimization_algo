91fecf89903a3344c0556c12124785b3c96d0aa0
Mark Kozlov <37026894+Ubermensch124@users.noreply.github.com>
2021-12-26 16:57:22 +0300

    0.0.10

--- optimizationalgo/optimizationalgo/__init__.py
+++ optimizationalgo/optimizationalgo/__init__.py
@@ -4,12 +4,20 @@
 
 Библиотека, реализующая оптимизационные алгоритмы для задач разного рода.
 В данный момент поддерживаются алгоритмы для задачи коммовояжёра.
-Для подробной информации пройдите в optimizationalgo.voyage
+Для подробной информации по этой задаче пройдите в optimizationalgo.voyage
+
+Доступные субпакеты
+---------------------
+voyage
+    Алгоритмы решения задачи коммивояжёра
 
 """
 from .voyage import simulated_annealing
 from .voyage import ants_colony
+from .voyage import create_visual
 
-__author__ = 'Mark Kozlov'
+__author__ = 'Марк Козлов, Вячеслав Есаков, Артём Радайкин, Александр Савостьянов, Лев Памбухчян'
 
-__version__ = "0.0.7"
+__version__ = "0.0.10"
+
+__all__ = ['ants_colony', 'simulated_annealing', 'create_visual']
